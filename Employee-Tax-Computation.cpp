63813cfd1e334ac2cbdd2adbf7d543bb173b0401
isnotfred <fredorlain5@gmail.com>
2024-11-29 16:45:48 +0800

    Employee-Tax-Computation.cpp

--- Employee-Tax-Computation.cpp
+++ Employee-Tax-Computation.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 using namespace std;
 
-//Function to calculate and display results
+//Function to calculate and display results (monthly salary, marital status, tax rate, salary tax)
 void displayResults (float salary, string maritalStatus, float taxRate) {
     cout << "\nEmployee Monthly Salary: " << fixed << setprecision(2) << salary << endl;
     cout << "Marital Status: " << maritalStatus << endl;
@@ -14,7 +14,7 @@ int main() {
     float salary, taxRate;
     char maritalStatus;
 
-    //Get input for marital status
+    //Input marital status
     cout << "Enter marital status (S for single / M for married): ";
     cin >> maritalStatus;
 
@@ -22,12 +22,9 @@ int main() {
     switch (maritalStatus) {
 
     //Check if marital status is 'Single'
-    case 's':
-    case 'S':
-        //Get monthly salary for 'Single'
+    case 's': case 'S':
+        //Input and validate salary
         cout << "Enter monthly salary (non-negative): ";  
-
-        //Validate input
         if (!(cin >> salary)) {
         cout << "Error: Invalid input! Please enter a number." << endl;
         cin.clear(); 
@@ -35,29 +32,21 @@ int main() {
         return 1;
         }
 
-        //Validate monthly salary     
+        //Check if non-negative and determine tax rate based on salary 
         if (salary >= 0) {
-            if (salary > 100000) {
-                displayResults(salary, "Single", 0.30);
-            } else if (salary <= 100000 && salary >= 50000) {
-                displayResults(salary, "Single", 0.20);
-            } else {
-                displayResults(salary, "Single", 0.10);
-            }
+            if (salary > 100000) displayResults(salary, "Single", 0.30);
+            else if (salary <= 100000 && salary >= 50000) displayResults(salary, "Single", 0.20);
+            else displayResults(salary, "Single", 0.10);
 
         //Error message for negative number
-        } else {
+        } else 
             cout << "Error: The salary must be non-negative value only!" << endl;
-        }
         break;
 
     //Check if marital status is 'Married'
-    case 'M':
-    case 'm':
-        //Get monthly salary for 'Married'
+    case 'M': case 'm':
+        //Input salary and validate input
         cout << "Enter monthly salary (non-negative): ";
-
-        //Validate input  
         if (!(cin >> salary)) {
         cout << "Error: Invalid input! Please enter a number." << endl;
         cin.clear(); 
@@ -65,20 +54,15 @@ int main() {
         return 1; 
         }
 
-        //Validate monthly salary
+        //Check if non-negative and determine tax rate based on salary
         if (salary >= 0) {
-            if (salary > 100000) {
-                displayResults(salary, "Married", 0.25);
-            } else if (salary <= 100000 && salary >= 50000) {
-                displayResults(salary, "Married", 0.15);
-            } else {
-                displayResults(salary, "Married", 0.05);
-            }
+            if (salary > 100000) displayResults(salary, "Married", 0.25);
+            else if (salary <= 100000 && salary >= 50000) displayResults(salary, "Married", 0.15);
+            else displayResults(salary, "Married", 0.05);
 
         //Error message for negative number
-        } else {
+        } else
             cout << "Error: The salary must be non-negative value only!" << endl;
-        }
         break;
 
     //Error message for invalid marital status
